3dd426bbef7e6d6bc6cf470a7fbc572d0ef7abb3
Rene Dellefont <renedellefont@gmail.com>
2020-03-19 09:30:59 -0400

    Update 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,23 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
-const dateFormat = require('./11ty/filters/date')
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const pluginTOC = require('eleventy-plugin-toc')
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
+const dateFormat = require('./11ty/filters/date')
+const sc = require('./11ty/shortcodes')
+
+const mdOptions = {
+  html: true,
+  breaks: true,
+  linkify: true,
+}
+
+// Anchor Rollover for Headings
+const mdAnchorOptions = {
+  permalink: true,
+  permalinkSymbol:
+    '<svg viewBox="0 0 20 21"><path d="M10 1.658L7.73 4.04c1.316-.275 2.994-.068 3.865.364l.494-.518c1.06-1.123 3.15-1.106 4.21 0 1.05 1.105 1.03 3.333 0 4.42l-3.16 3.317c-1.04 1.088-3.16 1.105-4.21 0-.38-.398-.63-.898-.76-1.417l-2.25 2.366c.26.45.56.864.92 1.244 2.1 2.21 6.3 2.228 8.4.017l3.18-3.333c2.1-2.21 2.1-6.632 0-8.842-2.11-2.21-6.32-2.21-8.42 0zM8.405 16.596l-.494.518c-1.06 1.123-3.15 1.106-4.21 0-1.05-1.105-1.03-3.333 0-4.42l3.16-3.317c1.04-1.088 3.16-1.105 4.21 0 .38.398.63.898.76 1.417l2.26-2.366c-.26-.45-.56-.864-.92-1.244-2.1-2.21-6.3-2.228-8.4-.017L1.58 10.5c-2.106 2.21-2.106 6.632 0 8.842 2.104 2.21 6.315 2.21 8.42 0l2.27-2.383c-1.316.27-2.994.06-3.865-.37z"></path></svg>',
+}
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
@@ -10,27 +27,43 @@ module.exports = function(eleventyConfig) {
     ]
   });
 
+  /*
+   * PLUGINS *
+  */
   eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(syntaxHighlight)
+  
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    // init callback lets you customize Prism
+    init: function({ Prism }) {
+      require('prism-svelte')
+    }
+  })
 
-  // Add a friendly date filter to nunjucks.
-  // Defaults to format of LLL d, y unless an alternate is passed as a parameter.
-  // {{ date | dateDisplay('OPTIONAL FORMAT STRING') }}
+  eleventyConfig.addPlugin(pluginTOC)
+
+  /*
+   * FILTERS *
+  */
   eleventyConfig.addFilter('dateDisplay', dateFormat)
   eleventyConfig.addFilter('htmlDateString', dateObject => dateFormat(dateObject, 'yyyy-LL-dd'))
-
 
   // Unsorted items (in whatever order they were added)
   eleventyConfig.addCollection("all", function(collection) {
     return collection.getAll();
   });
 
+  /*
+   * COLLECTIONS
+  */
   // Posts collection using glob
   eleventyConfig.addCollection('posts', function(collection) {
-    return collection.getFilteredByGlob('./src/posts/**')
+    return collection.getFilteredByGlob('./src/writing/**')
   })
 
-  eleventyConfig.addPassthroughCopy({ "src/_assets/images": "assets/images" });
+  eleventyConfig.setLibrary("md", markdownIt(mdOptions).use(markdownItAnchor, mdAnchorOptions));
+
+  eleventyConfig.addPassthroughCopy({ "src/_assets/images": "images" });
+  eleventyConfig.addPassthroughCopy("src/writing/**/*.(jpg|png)" );
 
   return {
     dir: {
@@ -39,5 +72,6 @@ module.exports = function(eleventyConfig) {
     },
     htmlTemplateEngine: 'njk',
     templateFormats: ['html', 'njk', 'md'],
+    markdownTemplateEngine: 'liquid'
   }
 }
