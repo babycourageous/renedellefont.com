54f3fab423a8d35b5cd0d673b56f5521076b59cd
Rene Dellefont <renedellefont@gmail.com>
2020-03-19 09:24:51 -0400

    Update date filter w/comment

--- 11ty/filters/date.js
+++ 11ty/filters/date.js
@@ -1,5 +1,11 @@
 const { format } = require('date-fns')
 
+// Add a friendly date filter to nunjucks.
+// Defaults to format of LLLL d, y unless an
+// alternate is passed as a parameter.
+// {{ date | friendlyDate('OPTIONAL FORMAT STRING') }}
+// List of supported tokens: https://moment.github.io/luxon/docs/manual/formatting.html#table-of-tokens
+
 module.exports = (dateObj, f = 'LLL d, y') => {
   return format(dateObj, f)
 }
