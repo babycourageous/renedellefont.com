b6fce76e0e2354bc7a9cb4fea4301bd7a8e052f3
Rene Dellefont <renedellefont@gmail.com>
2020-03-20 20:04:35 -0400

    Don't purge prism

--- postcss.config.js
+++ postcss.config.js
@@ -9,6 +9,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  whitelistPatternsChildren: [/^token/, /^pre/, /^code/],
 })
 
 module.exports = {
