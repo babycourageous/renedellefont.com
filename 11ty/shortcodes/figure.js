efb228cb8858f6da18d47640ca191a3c5a80d411
Rene Dellefont <renedellefont@gmail.com>
2020-03-23 21:36:24 -0400

    Update figure shortcode to reference images/writing

--- 11ty/shortcodes/figure.js
+++ 11ty/shortcodes/figure.js
@@ -1,6 +1,6 @@
 module.exports = (filename, alt = '', caption = '', ratio = '16/9') => {
   return `<figure><div style="--aspect-ratio: ${ratio};">
-  <img class="shadow" src="../images/${filename}" alt="${alt}" /></div>
+  <img class="shadow" src="/images/writing/${filename}" alt="${alt}" /></div>
   ${caption ? `<figcaption class="mt-1 text-gray-600 text-sm">${caption}</figcaption>` : ''}
   </figure>`
 }
